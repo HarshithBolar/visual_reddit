0d3dacbbdb162f8121a94f78a89bfee8bea3b119
HarshithBolar <harshith.bolar@gmail.com>
2019-10-28 21:31:21 +0530

    add reddit post title as tooltips to images

--- visual_reddit/reddit_api.py
+++ visual_reddit/reddit_api.py
@@ -6,15 +6,15 @@ class MyReddit:
     def __init__(self):
         self.reddit = praw.Reddit('visual-reddit', user_agent='visual_reddit_1.0')
 
-    def get_top_40_images(self, subreddit):
+    def get_top_images(self, subreddit):
         post_map = {}
         image_counter = 0
 
         for submission in self.reddit.subreddit(subreddit).hot(limit=None):
             if str(submission.url).endswith(self.valid_image_extensions):
-                post_map[str(submission)] = str(submission.url)
+                post_map[str(submission)] = (str(submission.url), submission.title)
                 image_counter += 1
-            if image_counter >= 40:
+            if image_counter >= 28:
                 break
 
         return post_map
--- visual_reddit/views.py
+++ visual_reddit/views.py
@@ -5,7 +5,7 @@ from . import reddit_api
 reddit_api = reddit_api.MyReddit()
 
 def index(request, subreddit='all'):
-    image_dict = reddit_api.get_top_40_images(subreddit)
+    image_dict = reddit_api.get_top_images(subreddit)
     template = loader.get_template('visual_reddit/index.html')
 
     context = {
