4ef90bd737e529e5c7e529d852c15a34d88b66f9
HarshithBolar <harshith.bolar@gmail.com>
2019-10-28 20:07:45 +0530

    index page, fetch top 40 images from r/all and display them

--- visual_reddit/reddit_api.py
+++ visual_reddit/reddit_api.py
@@ -1,5 +1,20 @@
 import praw
 
 class MyReddit:
+    valid_image_extensions = ('jpg', 'jpeg', 'png')
+
     def __init__(self):
         self.reddit = praw.Reddit('visual-reddit', user_agent='visual_reddit_1.0')
+
+    def get_top_40_images(self):
+        post_map = {}
+        image_counter = 0
+
+        for submission in self.reddit.subreddit('all').hot(limit=None):
+            if str(submission.url).endswith(self.valid_image_extensions):
+                post_map[str(submission)] = str(submission.url)
+                image_counter += 1
+            if image_counter > 40:
+                break
+
+        return post_map
--- visual_reddit/views.py
+++ visual_reddit/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
 from . import reddit_api
 
 reddit_api = reddit_api.MyReddit()
 
 def index(request):
-    return HttpResponse("Display top 40 images from r/all")
+    image_dict = reddit_api.get_top_40_images()
+    template = loader.get_template('visual_reddit/index.html')
+
+    context = {
+        'image_dict': image_dict
+    }
+
+    return HttpResponse(template.render(context, request))
